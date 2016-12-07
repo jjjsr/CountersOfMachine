89529e1c06f5a49f3958678151024c00cf27349e
jjjsr <koninipitufo@gmail.com>
2016-12-07 16:18:38 -0500

    Cambios para nuevos contadores a considerar

--- aggreCounters.java
+++ aggreCounters.java
@@ -13,25 +13,17 @@ import android.widget.Toast;
  */
 
 public class aggreCounters extends ActionBarActivity {
-    EditText             MachineId, TotalIn, TotalOut, HandPaid, Bill, Jackpot, TicketIn,TicketOut;
+    EditText             MachineId, TotalIn, TotalOut, Drop, Jackpot, CancelCredit, Bill, TicketIn,TicketOut, Bonus, Hopper;
     Button               SAVE;
     int                  machineId;
-    Long                 totalIn,totalOut,handPaid,bill,jackpot,ticketIn,ticketOut;
+    Long                 totalIn, totalOut, drop, jackpot, cancelCredit, bill, ticketIn,ticketOut, bonus, hopper;
     ConnectionDB         db;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insert_counters);
-
-        SAVE        = (Button)      findViewById(R.id.btn_save);
-        MachineId   = (EditText)    findViewById(R.id.actv_Id);
-        TotalIn     = (EditText)    findViewById(R.id.actv_TotalIn);
-        TotalOut    = (EditText)    findViewById(R.id.actv_TotalOut);
-        HandPaid    = (EditText)    findViewById(R.id.actv_HP);
-        Bill        = (EditText)    findViewById(R.id.actv_Bill);
-        Jackpot     = (EditText)    findViewById(R.id.actv_Jackpot);
-        TicketIn    = (EditText)    findViewById(R.id.actv_TicketIn);
-        TicketOut   = (EditText)    findViewById(R.id.actv_TicketOut);
+        InitialElementInterface();
+        PositionCursor();
 
         SAVE.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
@@ -59,23 +51,29 @@ public class aggreCounters extends ActionBarActivity {
                         return;
                     }while (TotalOut.getText().toString().isEmpty());
                 }
-                if (HandPaid.getText().toString().isEmpty()){
+                if (Drop.getText().toString().isEmpty()){
                     do {
-                        HandPaid.setError("Can not be null");
+                        Drop.setError("Can not be null");
                         return;
-                    }while (HandPaid.getText().toString().isEmpty());
-                }
-                if (Bill.getText().toString().isEmpty()){
-                    do {
-                        Bill.setError("Can not be null");
-                        return;
-                    }while (Bill.getText().toString().isEmpty());
+                    }while (Drop.getText().toString().isEmpty());
                 }
                 if (Jackpot.getText().toString().isEmpty()){
                     do {
                         Jackpot.setError("Can not be null");
                         return;
                     }while (Jackpot.getText().toString().isEmpty());
+                }
+                if (CancelCredit.getText().toString().isEmpty()){
+                    do {
+                        CancelCredit.setError("Can not be null");
+                        return;
+                    }while (CancelCredit.getText().toString().isEmpty());
+                }
+                if (Bill.getText().toString().isEmpty()){
+                    do {
+                        Bill.setError("Can not be null");
+                        return;
+                    }while (Bill.getText().toString().isEmpty());
                 }
                 if (TicketIn.getText().toString().isEmpty()){
                     do {
@@ -89,34 +87,65 @@ public class aggreCounters extends ActionBarActivity {
                         return;
                     }while (TicketOut.getText().toString().isEmpty());
                 }
+                if (Bonus.getText().toString().isEmpty()){
+                    do {
+                        Bonus.setError("Can not be null");
+                        return;
+                    }while (Bonus.getText().toString().isEmpty());
+                }
+                if (Hopper.getText().toString().isEmpty()){
+                    do {
+                        Hopper.setError("Can not be null");
+                        return;
+                    }while (Hopper.getText().toString().isEmpty());
+                }
                 AddCounters();
             }
         });
-        PositionCursor();
     }
     private void AddCounters(){
-            machineId   = Integer.parseInt(MachineId.getText().toString());
-            totalIn     = Long.parseLong(TotalIn.getText().toString());
-            totalOut    = Long.parseLong(TotalOut.getText().toString());
-            handPaid    = Long.parseLong(HandPaid.getText().toString());
-            bill        = Long.parseLong(Bill.getText().toString());
-            jackpot     = Long.parseLong(Jackpot.getText().toString());
-            ticketIn    = Long.parseLong(TicketIn.getText().toString());
-            ticketOut   = Long.parseLong(TicketOut.getText().toString());
+            machineId       = Integer.parseInt(MachineId.getText().toString());
+            totalIn         = Long.parseLong(TotalIn.getText().toString());
+            totalOut        = Long.parseLong(TotalOut.getText().toString());
+            drop            = Long.parseLong(Drop.getText().toString());
+            jackpot         = Long.parseLong(Jackpot.getText().toString());
+            cancelCredit    = Long.parseLong(CancelCredit.getText().toString());
+            bill            = Long.parseLong(Bill.getText().toString());
+            ticketIn        = Long.parseLong(TicketIn.getText().toString());
+            ticketOut       = Long.parseLong(TicketOut.getText().toString());
+            bonus           = Long.parseLong(Bonus.getText().toString());
+            hopper          = Long.parseLong(Hopper.getText().toString());
             db = new ConnectionDB(this);
-            db.addCounters(machineId, totalIn, totalOut, handPaid, bill, jackpot, ticketIn, ticketOut);
+            db.addCounters(machineId, totalIn, totalOut, cancelCredit, bill, jackpot, ticketIn, ticketOut);
             Toast.makeText(getBaseContext(), "Successfully added machine counters!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(aggreCounters.this, watchCounters.class);
             startActivity(intent);
+    }
+    private void InitialElementInterface(){
+        SAVE            = (Button)      findViewById(R.id.btn_save);
+        MachineId       = (EditText)    findViewById(R.id.actv_Id);
+        TotalIn         = (EditText)    findViewById(R.id.actv_TotalIn);
+        TotalOut        = (EditText)    findViewById(R.id.actv_TotalOut);
+        Drop            = (EditText)    findViewById(R.id.actv_Drop);
+        Jackpot         = (EditText)    findViewById(R.id.actv_Jackpot);
+        CancelCredit    = (EditText)    findViewById(R.id.actv_CancelCredit);
+        Bill            = (EditText)    findViewById(R.id.actv_Bill);
+        TicketIn        = (EditText)    findViewById(R.id.actv_TicketIn);
+        TicketOut       = (EditText)    findViewById(R.id.actv_TicketOut);
+        Bonus           = (EditText)    findViewById(R.id.actv_Bonus);
+        Hopper          = (EditText)    findViewById(R.id.actv_Hopper);
     }
     private void PositionCursor(){
         MachineId.setSelection(MachineId.getText().length());
         TotalIn.setSelection(TotalIn.getText().length());
         TotalOut.setSelection(TotalOut.getText().length());
-        HandPaid.setSelection(HandPaid.getText().length());
-        Bill.setSelection(Bill.getText().length());
+        Drop.setSelection(Drop.getText().length());
         Jackpot.setSelection(Jackpot.getText().length());
+        CancelCredit.setSelection(CancelCredit.getText().length());
+        Bill.setSelection(Bill.getText().length());
         TicketIn.setSelection(TicketIn.getText().length());
         TicketOut.setSelection(TicketOut.getText().length());
+        Bonus.setSelection(Bonus.getText().length());
+        Hopper.setSelection(Hopper.getText().length());
     }
 }
